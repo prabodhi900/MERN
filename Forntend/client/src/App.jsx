6499302fe67e1a6bca1d8d6b51bebebdf5b7ea7e
prabodhi900 <yhprabodhi2000@gmail.com>
2023-12-13 16:45:31 +0530

    modifing index.js adding mongo

--- Forntend/client/src/App.jsx
+++ Forntend/client/src/App.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
+import Profile from './Pages/Profile'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/profile" element={<Profile/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/signup" element={<SignUp/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/" element={<Home/>}></Route>
+      
+    </Routes>
+    </BrowserRouter>
   )
 }
+
+  
+  
